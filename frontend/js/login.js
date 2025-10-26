56b9651c5fa4f18e80c4e83910da11e68e1c47e9
Villanueva112 <villanueva11_juegos@hotmail.com>
2025-10-26 07:26:38 -0700

    Nuevo index y login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -26,3 +26,29 @@ $('#loginForm').on('submit', function(e) {
     }
   });
 });
+
+
+ $(document).ready(function() {
+            // visibilidad de contraseña
+            $('#togglePassword').click(function() {
+                const passwordField = $('#password');
+                const type = passwordField.attr('type') === 'password' ? 'text' : 'password';
+                passwordField.attr('type', type);
+                
+                
+                $(this).find('i').toggleClass('fa-eye fa-eye-slash');
+            });
+            
+            // iniciar sesión con Google
+            $('.btn-google').click(function() {
+                alert('Redirigiendo a Google para autenticación...');
+                // falta agregar esta cosa, suerte jareth 
+            });
+            
+            // Cuando le picas se cambia el color del borde
+            $('.form-control').focus(function() {
+                $(this).parent().addClass('focused');
+            }).blur(function() {
+                $(this).parent().removeClass('focused');
+            });
+        });
